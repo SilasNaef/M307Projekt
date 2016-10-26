dced45a24edb952908f9448adda3932f05a31bfd
Public <Public>
2016-10-26 11:03:30 +0200

    FICK DI SILAS

---
+++ Tag1/Chris/DeklarationVariablen.php
@@ -0,0 +1,17 @@
+<?php
+$a = 5;     /* Variable a wird "5" gesetzt */
+$b = 4;     /* Variable b wird "4" gesetzt */
+
+$c = $a + $b;     /* c wird nun gebildet aus a + b */
+
+echo $c;     /* c wird nun mit Hilfe von echo ausgegeben */
+
+
+$a = 1.1234 * 13400; /*Mit punkt Kommas abtrennen */
+echo '<p>1.1234 *  13400 = '. $a.'</p>';
+
+$a = 1;
+$A = -2;
+$b = $a + $A;     /* $b = 1 + (-2) */
+echo '<p> Ich bin ein '.$b. '</p>';
+?>
