f397af8a1a408486773db0e5bcafdb8df5b0531a
Public <Public>
2016-10-26 13:39:12 +0200

    Save

--- Tag1/Silas/GeradeUngeradeZahlen.php
+++ Tag1/Silas/GeradeUngeradeZahlen.php
@@ -3,23 +3,34 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <style>
+    td {
+      border: 1px solid black;
+    }
+    </style>
   </head>
   <body>
       <table>
+        <thead>
+        <th>Ungerade</th>
+        <th>Gerade</th>
+      </thead>
+      <tbody>
         <?php
           for ($i=0; $i <= 1000; $i++) {
-            if ($i%2 == 0) {
-              echo "<tr class='even'>";
-              echo "<td>" . $i ." ✈ ". "</td>";
+            if ($i % 2 == 0) {
+              echo "<tr class='Gerade'>";
+              echo "<td></td><td>" . $i ."</td>";
               echo "</tr>";
             }
             else {
-              echo "<tr>";
-              echo "<td>" . $i ." ✈ ". "</td>";
+              echo "<tr class='Ungerade'>";
+              echo "<td>" . $i ."</td><td></td>" ;
               echo "</tr>";
             }
           }
         ?>
+      </tbody>
       </table>
   </body>
 </html>
