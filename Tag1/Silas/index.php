945af772aac27a20ea36e234809a8c5ab9ce4183
Public <Public>
2016-10-26 10:44:19 +0200

    ChrisAdded

---
+++ Tag1/Silas/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+        $name  = 'silas';
+        $alter = 17;
+        $gebtag = '07.08.1999';
+
+        echo '<p>'. $name . '<p>';
+        echo '<p>'. $alter . '<p>';
+        echo '<p>'. $gebtag . '<p>';
+    ?>
+  </body>
+</html>
